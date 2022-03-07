a6d26763a631a17bd474748959ebb09a6d2f64eb
George Hett <georgehett95@gmail.com>
2022-03-07 16:33:51 +0000

    first html/css implementation using sinatra

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
+also_reload '/'
+after_reload do
+  puts 'reloaded'
+end
 
 get '/' do
-  "Hello!"
+  "Test!"
+end
+
+get '/secret' do
+  "Shhhhh..."
+end
+
+get '/greeting' do
+  "Bonjour!"
+end
+
+get '/cat' do
+  "<div style='border: dashed red'>
+  <img src=https://i.imgur.com/jFaSxym.png>
+  </div>"
 end
